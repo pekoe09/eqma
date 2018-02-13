35eca6779882ca9574e24ef75c14d91020eed4ad
Juha <juha.kangas@welho.com>
2018-02-13 23:20:54 +0200

    Added a couple of tests for POST /api/users

--- tests/users.test.js
+++ tests/users.test.js
@@ -41,6 +41,155 @@ describe('GET /api/users', () => {
   })
 })
 
+describe('POST /api/users', () => {
+  beforeEach(async () => {
+    await User.remove({})
+    const userObjects = initialUsers.map(user => new User(user))
+    const promiseArray = userObjects.map(user => user.save())
+    await Promise.all(promiseArray)
+  })
+
+  test('adds a user', async () => {
+    const newUser = {
+      username: 'newuser',
+      password: '12345',
+      firstName: 'New',
+      lastName: 'User',
+      status: 'user'
+    }
+
+    const usersBefore = await usersInDb()
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAfter = await usersInDb()
+    const usernames = usersAfter.map(user => user.username)
+
+    expect(usersAfter.length).toBe(usersBefore.length + 1)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('sets status default to user', async () => {
+    const newUser = {
+      username: 'newuser',
+      password: '12345',
+      firstName: 'New',
+      lastName: 'User'
+    }
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+
+    const usersAfter = await usersInDb()
+    const match = usersAfter.find(u => u.id.toString() === response.body._id)
+
+    expect(match.status).toEqual('user')
+  })
+
+  test('does not accept user without a username', async () => {
+    const newUser = {
+      password: '12345',
+      firstName: 'New',
+      lastName: 'User'
+    }
+
+    const usersBefore = await usersInDb()
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+
+    const usersAfter = await usersInDb()
+    expect(usersAfter.length).toBe(usersBefore.length)
+    expect(response.body).toEqual({ error: 'username is missing' })
+  })
+
+  test('does not accept user with an empty string as the username', async () => {
+    const newUser = {
+      username: '',
+      password: '12345',
+      firstName: 'New',
+      lastName: 'User'
+    }
+
+    const usersBefore = await usersInDb()
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+
+    const usersAfter = await usersInDb()
+    expect(usersAfter.length).toBe(usersBefore.length)
+    expect(response.body).toEqual({ error: 'username is missing' })
+  })
+
+  test('does not accept user with an existing username', async () => {
+    const newUser = {
+      username: initialUsers[1].username,
+      password: '12345',
+      firstName: 'New',
+      lastName: 'User'
+    }
+
+    const usersBefore = await usersInDb()
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+
+    const usersAfter = await usersInDb()
+    expect(usersAfter.length).toBe(usersBefore.length)
+    expect(response.body).toEqual({ error: 'username is already in use' })
+  })
+
+  test('does not accept user without a password', async () => {
+    const newUser = {
+      username: 'newuser',
+      firstName: 'New',
+      lastName: 'User'
+    }
+
+    const usersBefore = await usersInDb()
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+
+    const usersAfter = await usersInDb()
+    expect(usersAfter.length).toBe(usersBefore.length)
+    expect(response.body).toEqual({ error: 'password is missing' })
+  })
+
+  test('does not accept user with an empty string as the password', async () => {
+    const newUser = {
+      username: 'newuser',
+      password: '',
+      firstName: 'New',
+      lastName: 'User'
+    }
+
+    const usersBefore = await usersInDb()
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+
+    const usersAfter = await usersInDb()
+    expect(usersAfter.length).toBe(usersBefore.length)
+    expect(response.body).toEqual({ error: 'password is missing' })
+  })
+})
+
 afterAll(() => {
   server.close()
 })
