ece9e04c99bbd7e595e1106211b0075cfe187a55
pekoe09 <juha.kangas@welho.com>
2018-04-15 19:57:58 +0300

    Customer message deleting from details view

--- react-ui/src/components/customerMessages/customerMessageEdit.js
+++ react-ui/src/components/customerMessages/customerMessageEdit.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { Form, Input, Select, Button, Label } from 'semantic-ui-react'
+import { Form, Input, Select, Button, Label, Confirm } from 'semantic-ui-react'
 import moment from 'moment'
 import ViewHeader from '../structure/viewHeader'
 import LinkButton from '../structure/linkButton'
-import { updateCustomerMessage } from '../../reducers/customerMessageReducer'
+import { updateCustomerMessage, removeCustomerMessage } from '../../reducers/customerMessageReducer'
+import { addUIMessage } from '../../reducers/uiMessageReducer'
 
 const formStyle = {
   marginTop: 10
@@ -24,7 +25,8 @@ class CustomerMessageEdit extends React.Component {
     handler: this.props.initialMessage.handler
       ? this.props.initialMessage.handler
       : this.props.user,
-    reply: this.props.initialMessage.reply
+    reply: this.props.initialMessage.reply,
+    openDeleteConfirm: false
   }
 
   handleChange = (event) => {
@@ -41,6 +43,21 @@ class CustomerMessageEdit extends React.Component {
     this.props.history.push('/customermessages')
   }
 
+  handleRemove = async () => {
+    this.setState({ openDeleteConfirm: true })
+  }
+
+  handleConfirmedRemove = async () => {
+    const name = this.props.initialMessage.name
+    await this.props.removeCustomerMessage(this.props.initialMessage._id)
+    this.props.addUIMessage(`Message from user ${name} deleted`, 'success', 10)
+    this.props.history.push('/customermessages')
+  }
+
+  handleCancelledRemove = () => {
+    this.setState({ openDeleteConfirm: false })
+  }
+
   replyLabel = () => {
     if (this.props.initialMessage.replySent) {
       return (
@@ -52,41 +69,54 @@ class CustomerMessageEdit extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <ViewHeader text={`Customer message from ${this.props.initialMessage.name}`} />
-        <LinkButton text={'Back to message list'} to={'/customermessages'} type={'default'} />
-        <Form style={formStyle} onSubmit={this.handleSubmit}>
-          <Form.Field>
-            <label>Customer name</label>
-            <p>{this.props.initialMessage.name}</p>
-          </Form.Field>
-          <Form.Field>
-            <label>Customer email</label>
-            <p>{this.props.initialMessage.email}</p>
-          </Form.Field>
-          <Form.Field>
-            <label>Message sent</label>
-            <p>{moment(this.props.initialMessage.sent).format('MM/DD/YYYY HH:mm')}</p>
-          </Form.Field>
-          <Form.Field>
-            <label>Message</label>
-            <p>{this.props.initialMessage.message}</p>
-          </Form.Field>
-          <Form.Field>
-            <label>Handler</label>
-            <p>{`${this.state.handler.firstName} ${this.state.handler.lastName}`}</p>
-          </Form.Field>
-          <Form.Field>
-            <Label style={labelStyle}>{this.replyLabel()}</Label>
-            <textarea value={this.state.reply} onChange={this.handleChange} rows={5} />
-          </Form.Field>
-          <Form.Field>
-            <Button primary>Send reply</Button>
-          </Form.Field>
-        </Form>
-      </div>
-    )
+    if (this.props.initialMessage) {
+      return (
+        <div>
+          <ViewHeader text={`Customer message from ${this.props.initialMessage.name}`} />
+          <LinkButton text='Back to message list' to='/customermessages' type='default' />
+          <Button negative onClick={this.handleRemove}>Delete</Button>
+          <Confirm
+            open={this.state.openDeleteConfirm}
+            header='Deleting a customer message'
+            content={`Deleting message from ${this.props.initialMessage.name}. The operation is permanent; are you sure?`}
+            confirmButton='Yes, delete'
+            onConfirm={this.handleConfirmedRemove}
+            onCancel={this.handleCancelledRemove}
+          />
+          <Form style={formStyle} onSubmit={this.handleSubmit}>
+            <Form.Field>
+              <label>Customer name</label>
+              <p>{this.props.initialMessage.name}</p>
+            </Form.Field>
+            <Form.Field>
+              <label>Customer email</label>
+              <p>{this.props.initialMessage.email}</p>
+            </Form.Field>
+            <Form.Field>
+              <label>Message sent</label>
+              <p>{moment(this.props.initialMessage.sent).format('MM/DD/YYYY HH:mm')}</p>
+            </Form.Field>
+            <Form.Field>
+              <label>Message</label>
+              <p>{this.props.initialMessage.message}</p>
+            </Form.Field>
+            <Form.Field>
+              <label>Handler</label>
+              <p>{`${this.state.handler.firstName} ${this.state.handler.lastName}`}</p>
+            </Form.Field>
+            <Form.Field>
+              <Label style={labelStyle}>{this.replyLabel()}</Label>
+              <textarea value={this.state.reply} onChange={this.handleChange} rows={5} />
+            </Form.Field>
+            <Form.Field>
+              <Button primary>Send reply</Button>
+            </Form.Field>
+          </Form>
+        </div>
+      )
+    } else {
+      return <div></div>
+    }
   }
 }
 
@@ -100,5 +130,9 @@ const mapStateToProps = (store, ownProps) => {
 
 export default withRouter(connect(
   mapStateToProps,
-  { updateCustomerMessage }
+  {
+    updateCustomerMessage,
+    removeCustomerMessage,
+    addUIMessage
+  }
 )(CustomerMessageEdit))
